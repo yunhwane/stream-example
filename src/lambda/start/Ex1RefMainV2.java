76df238cd61aad94c3a675e3e4bbf7b58071a346
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2025-04-01 00:04:08 +0900

    Refactor Ex1RefMainV2 to use lambda expressions for dice roll and sum calculations

--- src/lambda/start/Ex1RefMainV2.java
+++ src/lambda/start/Ex1RefMainV2.java
@@ -6,6 +6,9 @@ import java.util.Random;
 
 /**
  * 익명 클래스 사용
+ * 자바에서 메서드의 매개변수에 인수로 전달할 수 있는 경우는 크게 2가지이다.
+ * int, double, 과 같은 타입
+ * 참조형 타입(인스턴스)
  */
 public class Ex1RefMainV2 {
 
@@ -22,27 +25,20 @@ public class Ex1RefMainV2 {
     }
 
 
+    /**
+     * 람다를 사용하면 매우 간편하게 코드블럭을 직접 정의하고 전달하는 것을 볼 수 있음
+     */
     public static void main(String[] args) {
-        Procedure dice = new Procedure() {
-            @Override
-            public void run() {
-                int randomValue = new Random().nextInt(6) + 1;
-                System.out.println("주사위 눈: " + randomValue);
+        hello(() -> {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 눈: " + randomValue);
+        });
+        hello(() -> {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += i;
             }
-        };
-
-        Procedure sum = new Procedure() {
-            @Override
-            public void run() {
-                int sum = 0;
-                for (int i = 1; i <= 100; i++) {
-                    sum += i;
-                }
-                System.out.println("1부터 100까지의 합: " + sum);
-            }
-        };
-
-        hello(dice);
-        hello(sum);
+            System.out.println("1부터 100까지의 합: " + sum);
+        });
     }
 }
